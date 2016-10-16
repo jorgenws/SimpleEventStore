2ba66c4cce8bb5cc6369bdbe87a4afb062bac364
jorgenws <jorgen.wang.svendsen@gmail.com>
2016-10-16 22:10:56 +0200

    added reminder

--- EventStore/IEventRepository.cs
+++ EventStore/IEventRepository.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace SimpleEventStore
 {
+    //Find a way to communicate that you need to serial ids in increasing order and 
+    //that you must set the event to the serial id it gets persisted with
+    //when implementing this interface
     public interface IEventRepository : IDisposable
     {
         bool WriteEvents(List<EventTransaction> eventTransaction);
